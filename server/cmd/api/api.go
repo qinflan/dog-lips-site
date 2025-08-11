1a9674b0a6e920904a6e0c7da17ac6fcf551d83a
qinflan <158785365+qinflan@users.noreply.github.com>
2025-08-11 02:10:49 -0400

    connect postgres db

--- server/cmd/api/api.go
+++ server/cmd/api/api.go
@@ -1,10 +1,12 @@
 package api
+
 import (
 	"net/http"
 	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v5"
 )
 
-func NewRouter() *mux.Router {
+func NewRouter(db *pgx.Conn) *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
 	"net/http"
-	
-	"github.com/qinflan/dog-lips-site/server/internal/api"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+	"github.com/qinflan/dog-lips-site/server/cmd/api"
 )
 
 func main() {
-	router := api.NewRouter()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	
+	connStr := os.Getenv("DATABASE_URL")
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer conn.Close(context.Background())
 
-	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	if conn.Ping(context.Background()) == nil {
+		log.Println("Connected to the database successfully!")
+	}
+
+	router := api.NewRouter(conn)
+
+	log.Println("Starting server on :9090")
+	if err := http.ListenAndServe(":9090", router); err != nil {
+		log.Fatalf("Failed to start server: %v\n", err)
+	}
 }
