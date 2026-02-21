6a8dae54d1556944cf4b757b27a520c83f481eaa
qinflan <158785365+qinflan@users.noreply.github.com>
2026-02-20 23:27:24 -0500

    fix: cors for local and prod

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -46,8 +46,7 @@ func main() {
 	router := api.NewRouter(app)
 
 	corsRouter := handlers.CORS(
-		handlers.AllowedOrigins([]string{os.Getenv("ALLOWED_ORIGIN_PROD")}),
-		handlers.AllowedOrigins([]string{os.Getenv("ALLOWED_ORIGIN_LOCAL")}),
+		handlers.AllowedOrigins([]string{os.Getenv("ALLOWED_ORIGIN_PROD"), os.Getenv("ALLOWED_ORIGIN_LOCAL")}),
 		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"}),
 		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
 	)(router)
